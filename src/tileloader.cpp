6f504f1168f775b37034bdb23021f14a7706c586
Dennis Nienhüser <dennis@nienhueser.de>
2017-04-16 14:24:55 +0200

    Set a HTTP user agent header
    Some tile servers (e.g. openstreetmap.org) require a valid user agent.
    Fixes using http://a.tile.openstreetmap.org/{z}/{x}/{y}.png as tile url
    (Object URI).

--- src/tileloader.cpp
+++ src/tileloader.cpp
@@ -120,7 +120,9 @@ void TileLoader::start() {
       } else {
         const QUrl uri = uriForTile(x, y);
         //  send request
-        const QNetworkRequest request = QNetworkRequest(uri);
+        QNetworkRequest request = QNetworkRequest(uri);
+        request.setHeader(QNetworkRequest::UserAgentHeader,
+                          "rviz_satellite/0.0.2 (+https://github.com/gareth-cross/rviz_satellite)");
         QNetworkReply *rep = qnam_->get(request);
         emit initiatedRequest(request);
         tiles_.push_back(MapTile(x, y, zoom_, rep));
