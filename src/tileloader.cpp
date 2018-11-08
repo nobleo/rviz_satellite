5178c35497a7b7c110ad8639b626487c45425de9
Andre Schröder <andre.schroeder@tomtom.com>
2018-11-08 16:41:36 +0100

    1.0.0

--- src/tileloader.cpp
+++ src/tileloader.cpp
@@ -121,7 +121,7 @@ void TileLoader::start() {
         const QUrl uri = uriForTile(x, y);
         //  send request
         QNetworkRequest request = QNetworkRequest(uri);
-        auto const userAgent = QByteArray("rviz_satellite/0.0.2 (+https://github.com/gareth-cross/rviz_satellite)");
+        auto const userAgent = QByteArray("rviz_satellite/1.0.0 (+https://github.com/gareth-cross/rviz_satellite)");
         request.setRawHeader(QByteArray("User-Agent"), userAgent);
         QNetworkReply *rep = qnam_->get(request);
         emit initiatedRequest(request);
