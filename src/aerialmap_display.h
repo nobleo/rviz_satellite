df73f4773b0baaac74fbb0c097fe735363d43034
Andre Schröder <andre.schroeder@tomtom.com>
2018-12-04 10:15:33 +0100

    Make license more visible

--- src/aerialmap_display.h
+++ src/aerialmap_display.h
@@ -1,12 +1,16 @@
-/*
- * AerialMapDisplay.h
- *
- *  Copyright (c) 2014 Gaeth Cross. Apache 2 License.
- *
- *  This file is part of rviz_satellite.
- *
- *	Created on: 07/09/2014
- */
+/* Copyright 2014 Gareth Cross
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 
 #ifndef AERIAL_MAP_DISPLAY_H
 #define AERIAL_MAP_DISPLAY_H
