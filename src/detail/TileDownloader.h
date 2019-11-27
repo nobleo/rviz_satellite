02db2de0a34d0387803b38eb2f99b0d4e496f9aa
Stefan Kaiser <Stefan.Kaiser@tomtom.com>
2019-11-27 18:48:57 +0100

    Set tile fetching preference to cache
    By default, QNetworkRequest uses a "prefer network" (CacheLoadControl::PreferNetwork) policy for querying data. Since we rather want to avoid downloading the same tile images, we set it to "prefer disk cache" (QNetworkRequest::PreferCache). See https://doc.qt.io/qt-5/qnetworkrequest.html for details.

--- src/detail/TileDownloader.h
+++ src/detail/TileDownloader.h
@@ -66,12 +66,16 @@ public:
   void loadTile(TileId const& tileId)
   {
     // see https://foundation.wikimedia.org/wiki/Maps_Terms_of_Use#Using_maps_in_third-party_services
-    QNetworkRequest request(QUrl(QString::fromStdString(tileURL(tileId))));
+    auto const requestUrl = QUrl(QString::fromStdString(tileURL(tileId)));
+    ROS_DEBUG_STREAM_NAMED("rviz_satellite", "requesting tile from URL: " << requestUrl.toString().toStdString());
+    
+    QNetworkRequest request(requestUrl);
     char constexpr agent[] = "rviz_satellite/" RVIZ_SATELLITE_VERSION " (+https://github.com/gareth-cross/"
                              "rviz_satellite)";
     request.setHeader(QNetworkRequest::KnownHeaders::UserAgentHeader, agent);
     QVariant variant;
     variant.setValue(tileId);
+    request.setAttribute(QNetworkRequest::CacheLoadControlAttribute, QNetworkRequest::CacheLoadControl::PreferCache);
     request.setAttribute(QNetworkRequest::User, variant);
     manager->get(request);
   }
@@ -85,13 +89,22 @@ public slots:
     QUrl const url = reply->url();
     if (reply->error())
     {
-      ROS_ERROR_STREAM(reply->errorString().toStdString());
+      ROS_ERROR_STREAM("Got error when loading tile: " << reply->errorString().toStdString());
       errorRates.issueError(tileId.tileServer);
       return;
     }
     else
     {
       errorRates.issueSuccess(tileId.tileServer);
+    }
+
+    // log if tile comes from cache or web
+    QVariant fromCache = reply->attribute(QNetworkRequest::SourceIsFromCacheAttribute);
+    if (fromCache.toBool()) {
+      ROS_DEBUG_STREAM_NAMED("rviz_satellite", "Loaded tile from cache (" << url.toString().toStdString() << ")");
+    }
+    else {
+      ROS_DEBUG_STREAM_NAMED("rviz_satellite", "Loaded tile from web (" << url.toString().toStdString() << ")");
     }
 
     QImageReader reader(reply);
